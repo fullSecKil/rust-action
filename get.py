164d3cf050873fc81ca438f5a9478cd389926149
Dusk <33350026+fullSecKil@users.noreply.github.com>
2024-02-23 10:43:47 +0800

    Update get.py

--- get.py
+++ get.py
@@ -2,7 +2,7 @@ import http.client
 import json
 
 conn = http.client.HTTPSConnection("api.devnet.solana.com")
-payload = json.dumps({
+payload1 = json.dumps({
   "method": "requestAirdrop",
   "jsonrpc": "2.0",
   "params": [
@@ -11,10 +11,27 @@ payload = json.dumps({
   ],
   "id": "c5935154-73a1-4712-9c08-c392d216dd22"
 })
+
+payload2 = json.dumps({
+  "jsonrpc": "2.0",
+  "id": "96dec5c4-4e6e-447e-bcf3-2bdebf9d282d",
+  "method": "requestAirdrop",
+  "params": [
+    "AuwZZwWTVMGxf3VWBo7rGgEoYJSwvzssaC7Tco12z5gr",
+    2000000000
+  ]
+})
+
+arrays = [payload1, payload2]
+
 headers = {
   'Content-Type': 'application/json'
 }
-conn.request("POST", "/", payload, headers)
-res = conn.getresponse()
-data = res.read()
-print(data.decode("utf-8"))
+
+for payload in arrays:
+  conn.request("POST", "/", payload, headers)
+  res = conn.getresponse()
+  data = res.read()
+  print(data.decode("utf-8"))
+  print("-------------------------------------\n")
+
