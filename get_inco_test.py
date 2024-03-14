26959f49a300e883d4bec05ff75d4693a748d884
Dusk <33350026+fullSecKil@users.noreply.github.com>
2024-03-14 11:19:05 +0800

    Update get_inco_test.py
    update

--- get_inco_test.py
+++ get_inco_test.py
@@ -7,7 +7,15 @@ payload1 = json.dumps({
     "address": "0x1d2d4062F3bF9789bE550F08B7b9Add010EEb5D8"
 })
 
-arrays = [payload1, payload1]
+payload2 = json.dumps({
+    "address": "0x7bA0f41AFb392f647e7E64dDad8912fa6F7a76bd"
+})
+
+payload3 = json.dumps({
+    "address": "0x00B2fad42707d1A632f30D8C76e7f43e5337b0c6"
+})
+
+arrays = [payload1, payload1, payload2 , payload2, payload3, payload3]
 
 headers = {
   'Content-Type': 'application/json'
