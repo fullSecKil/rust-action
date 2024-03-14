69fc5a2e91633efaa0b65c0703855dcecfcc217f
Dusk <33350026+fullSecKil@users.noreply.github.com>
2024-03-14 11:01:26 +0800

    Create get_inco_test.py
    add inco_test script

---
+++ get_inco_test.py
@@ -0,0 +1,22 @@
+import http.client
+import json
+import time
+
+conn = http.client.HTTPSConnection("faucet.testnet.inco.org/api/get-faucet")
+payload1 = json.dumps({
+    "address": "0x1d2d4062F3bF9789bE550F08B7b9Add010EEb5D8"
+})
+
+arrays = [payload1]
+
+headers = {
+  'Content-Type': 'application/json'
+}
+
+for payload in arrays:
+  conn.request("POST", "/", payload, headers)
+  res = conn.getresponse()
+  data = res.read()
+  print(data.decode("utf-8"))
+  print("-------------------------------------\n")
+  time.sleep(2)
